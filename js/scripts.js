619f27246beb32c1a327a0520638ca70c8db8bdc
Kari Fuller <kari.c.fuller@gmail.com>
2018-06-23 08:51:32 -0700

    update function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 var emptyArray = []
 
-var numberEntered = function(number) {
-  if (number % 15 === 0){
-    emptyArray.push("pingpong");
-    $(".result").text(emptyArray);
-  } else if (number % 5 === 0) {
-    emptyArray.push("pong");
-    $(".result").text(emptyArray);
-  } else if (number % 3 === 0) {
-    emptyArray.push("ping");
-    $(".result").text(emptyArray)
-  } else {
-    emptyArray.push(number);
-    $(".result").text(emptyArray);
-  }
-};
+function getResponse(number) {
+    if (number % 15 === 0) {
+        return "pingpong";
+    }
+    else if (number % 5 === 0) {
+        return "pong";
+    }
+    else if (number % 3 === 0) {
+        return "ping";
+    }
+    else {
+        return number.toString();
+    }
+}
+
 
 $(document).ready(function() {
   $form("pingpong").submit(function(event){
